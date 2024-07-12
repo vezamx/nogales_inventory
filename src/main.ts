0f35312b64e5726a1742f6902bc5b14c74618b62
Jonathan Marquez <zomodan2306@gmail.com>
2024-07-12 13:43:24 -0600

    fix(cors): added cors validation

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,16 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
+require('dotenv').config();
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(new ValidationPipe());
+  app.enableCors({
+    origin: process.env.FRONTEND_URL,
+    allowedHeaders:
+      'Origin, X-Requested-With, Content-Type, Accept, Authorization',
+  });
   await app.listen(process.env.PORT || 4242);
 }
 bootstrap();
